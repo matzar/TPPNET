f0106f607d818bfe837e2d94045effd0607fcbfb
matt360 <matzaremba@icloud.com>
2018-08-09 15:37:13 +0200

    Cube moves on Server and Client because it is a Replicated Actor - SetReplicates(true) - and has a Replicated property - SetReplicateMovement(true)

--- Source/TTPNET/MovingPlatform.cpp
+++ Source/TTPNET/MovingPlatform.cpp
@@ -1,5 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+/**
+* The Actors are the key bit of replication.
+* The Actor will be updated on the Client, from the Server,
+* only if the Actor is a Replicated Actor and there's a Replicated propert on that actor.
+*/
+
 #include "MovingPlatform.h"
 
 AMovingPlatform::AMovingPlatform()
@@ -7,6 +13,17 @@ AMovingPlatform::AMovingPlatform()
 	PrimaryActorTick.bCanEverTick = true;
 
 	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
 }
 
 void AMovingPlatform::Tick(float DeltaTime)
--- Source/TTPNET/MovingPlatform.h
+++ Source/TTPNET/MovingPlatform.h
@@ -1,11 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-/**
-* The Actors are the key bit of replication.
-* The Actor will be updated on the Client, from the Server,
-* only if the Actor is a Replicated Actor and there's a Replicated propert on that actor.
-*/
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -22,6 +16,8 @@ class TTPNET_API AMovingPlatform : public AStaticMeshActor
 
 public:
 	AMovingPlatform();
+
+	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
 
