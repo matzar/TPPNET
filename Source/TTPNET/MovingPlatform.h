c49e2a4246939c56cdac9ac5ef15e5ba3c4c9d1e
matt360 <matzaremba@icloud.com>
2018-08-09 15:05:55 +0200

    has authority

--- Source/TTPNET/MovingPlatform.h
+++ Source/TTPNET/MovingPlatform.h
@@ -1,5 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+/**
+* The Actors are the key bit of replication.
+* The Actor will be updated on the Client, from the Server,
+* only if the Actor is a Replicated Actor and there's a Replicated propert on that actor.
+*/
+
 #pragma once
 
 #include "CoreMinimal.h"
