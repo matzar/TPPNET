f264e48a6a9e92936a611bbaf9395f46a2fb613a
matt360 <matzaremba@icloud.com>
2018-08-13 17:06:56 +0200

    Update: small changes

--- Source/TTPNET/TPPNETGameInstance.cpp
+++ Source/TTPNET/TPPNETGameInstance.cpp
@@ -19,14 +19,12 @@ void UTPPNETGameInstance::Host()
 	UEngine* Engine = GetEngine();
 
 	if (!ensure(Engine != nullptr)) return;
-
 	Engine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, TEXT("Hosting"));
 
 	UWorld* World = GetWorld();
-
 	if (!ensure(World != nullptr)) return;
 
-	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap");
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
 }
 
 void UTPPNETGameInstance::Join(const FString& Address)
@@ -34,6 +32,10 @@ void UTPPNETGameInstance::Join(const FString& Address)
 	UEngine* Engine = GetEngine();
 
 	if (!ensure(Engine != nullptr)) return;
-
 	Engine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
 }
